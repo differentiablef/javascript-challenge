afd1a4e10f470cdfa8c9100a68133d84d07216a6
differentiablef <differentiablef@gmail.com>
2019-09-27 03:13:53 -0400

    build_filter and css fixup

--- UFO-level2/static/js/app.js
+++ UFO-level2/static/js/app.js
@@ -1,86 +1,308 @@
-// from data.js
-var tableData = data;
+/* 
+ * Name: build_filter
+ * Synop: construct and return a function which takes a single
+ *    parameter, `entry` and returns whether or not `entry` satisfies
+ *    the conditions represented by `conditions`
+ */
+function build_filter(conditions)
+{
+    let atomic_formula, preprocessed, filter;
+    
+    atomic_formula=(attr,val)=>
+        `(entry.${attr}.toUpperCase() === "${val.toString().toUpperCase()}")`;
+
+    // preprocess `conditions` into an appropriately grouped
+    //   family of atomic statements
+    preprocessed = Object.entries(conditions)
+        .map(([key, val]) => val.map(entry => atomic_formula(key, entry)));
+
+    // assemble statement with free variable `entry`
+    //   which determines whether or not entry meets the filtering
+    //   criteria
+    filter = preprocessed
+        .filter(x => x.length>0)
+        .map(disjunct => `(${disjunct.join('||')})`).join('&&');
+
+    // return anonymous function testing assembled statement
+    return eval('entry => '+(filter.length === 0 ? 'true' : filter));
+}
+
 
 /*
- * Name: populate_table
- * Synop:
- *    clear the body of the table with id `ufo-table` and 
- *    populate with appropriate data from `tableData`
- *
- * Note: 
- *    if `date` is not undefined, then the function check if it is a valid
- *    date and use it to filter the results
+ * Name: extract_values
+ * Synop: processes the array `objects` (assumed to 
+ *    all have the same set of attributes) and extracts the _set_'s
+ *    of values which occur for each attribute.
  */
-function populate_table
-(date=undefined) {
-    let ubody=d3.select("#ufo-table>tbody"), // select table body
-        filter_func=(x) => true, // set default filter function
-        dt=new Date(date); // attempt to parse `date`
+function extract_values(objects)
+{
+    let values = {};
     
-    // if not undefined, check that the date is valid
-    if( date !== undefined ) {
-        if(dt.getYear() === NaN) {
-            // TODO: inform user of issue with date
-            throw("Invalid date");
-        } else {
-            filter_func = function(entry) {
-                    let m = dt.getMonth()+1,
-                        d = dt.getDate(),
-                        y = dt.getFullYear();
-                    
-                    return entry.datetime === `${m}/${d}/${y}`;
-            };
-        }
-    }
-    
-    // clear table body
-    ubody.html('');
-
-    // filter table data appropriately
-    let tdata = tableData.filter( filter_func );
-
-    // populate table with contents of `tdata`
-    tdata.forEach(
-        (entry) => {
-            let row = ubody.append('tr');
-            Object
-                .entries(entry)
-                .forEach(
-                    ([key, value]) => {
-                        let cell = row.append('td');
-                        if(value !== undefined)
-                            cell.html(value.toString()
-                                      .toUpperCase());
-                    });
+    // extract unique attribute values
+    objects.forEach(
+        x => {
+            Object.entries(x).forEach(
+                y => {
+                    if(y[0] in values) {
+                        let idx = values[y[0]].indexOf(y[1])
+                        if( idx == -1 )
+                            values[y[0]].push(y[1]);
+                    } else
+                        values[y[0]] = [y[1]];
+                });
         });
 
-    return;
+    // sort resulting arrays
+    Object.keys(values).forEach(
+        k => values[k].sort()
+    );
+
+    // return object
+    return values;
+}
+
+
+/*
+ * Name: update_table
+ * Synop:
+ *    clear body of table with id `ufo-table` and 
+ *    populate with appropriately filtered data from `tableData`
+ */
+function update_table()
+{
+    let body, valid_entry;
+
+    // select table body
+    body = d3.select("#ufo-table>tbody");
+
+    // clear table body
+    body.html('');
+
+    // build function to filter data
+    valid_entry =
+        build_filter(conditions);
+    
+    // filter data using currently selected conditions
+    let valid = data.filter( valid_entry );
+
+    // populate table with contents of `valid`
+    valid.forEach(
+        (entry) => {
+            let row = body.append('tr');
+            Object.entries(entry).forEach(
+                ([key, value]) => {
+                    let cell = row.append('td');
+                    cell.html(value.toString().toUpperCase());
+                });
+        });
 }
 
 
 /* 
- * Name: filter_button_onclick
+ * Name: apply_filter_onclick
  * Synop:
  *    handles the `click` event emitted for button with id `filter-btn`.
- *    calls `populate_table` with contents of the input element with
- *    id `datetime` being passed as the `date` parameter
+ *    calls `populate_table`
  */
-function filter_button_onclick
-() {
+function apply_filter_onclick()
+{
     // prevent default behavior 
     d3.event.preventDefault();
     
-    // extract value of `#datetime` element
-    let dvalue = d3.select('#datetime').property('value');
-    
-    // call populate_table passing `dvalue` as `date`
-    populate_table(dvalue);
+    // update the contents of the table
+    update_table();
 }
 
-// setup event handlers
-d3.select("#filter-btn").on('click', filter_button_onclick);
-d3.select('form').on('submit', filter_button_onclick);
+/* 
+ * Name: menu_option_onclick
+ * Synop: updates the appropriate attribute of `conditions`
+ *     using the 'data-key' and 'data-value' attributes of the 
+ *     event target.
+ */
+function menu_option_onclick()
+{
+    // prevent default behavior 
+    d3.event.preventDefault();
+
+    // select target and extract attributes
+    let target = d3.select(this),
+        key = target.attr('data-key'),
+        val = target.attr('data-value'),
+        checkbox = target.select('input'),
+        idx = conditions[key].indexOf(val);
+
+    // is this condition already selected?
+    if (idx > -1) {
+        // if so, then unset it and remove it from `conditions[key]`
+        conditions[key].splice(idx, 1);
+        setTimeout(x => checkbox.property( 'checked', false ), 0);
+    } else {
+        // if not, then set it and update `conditions[key]`
+        conditions[key].push(val);
+        setTimeout(x => checkbox.property( 'checked', true ), 0);
+    }
+
+    // redraw is needed
+    target.dispatch('blur');
+    
+    return false;
+}
+
+
+
+var data = data; /* from data.js */
+
+var values = {},     /* Holds sets of values which occur as attributes in data*/
+    controls = {},   /* Holds display labels and filter panel elements */
+    conditions = {}; /* Holds conjunctive normal form of filtering criteria */
+
+/*
+ * The CNF representation we'll use is vaugely as follows, arrays represent
+ *   collections of conditions which will be joined together with an 'or',
+ *   while the attributes of the object correspond to compound statements 
+ *   and are joined together by 'and'
+ * (see function `build_filter` for details.) 
+ */
+
+conditions = { datetime: [],
+               city: [],
+               state: [],
+               country: [],
+               shape: [] };
+
+/*
+ * Collects values witnessed stored in some attribute 
+ *   of a member of `data`
+ */
+
+values = extract_values( data );
+
+/*
+ * Describes the controls which will be part of 
+ *   the form used to specify conditions used to filter `data`
+ */
+
+controls = [{ label: 'Date:',
+              key: 'datetime',
+              type: 'text',
+              placeholder: '1/11/2010' },
+            
+            { label: 'City:',
+              key: 'city',
+              type: 'text',
+              placeholder: 'Charlotte' },
+
+            { label: 'State',
+              key: 'state',
+              type: 'choices',
+              values: values.state },
+            
+            { label: 'Country',
+              key: 'country',
+              type: 'choices',
+              values: values.country },
+      
+            { key: 'shape',
+              type: 'choices',
+              values: values.shape,
+              label: 'Shape' }];
+                   
+                  
+var filters = d3.select('#filters');
+
+/*
+item = filters.append('li')
+.attr('id', 'datetime')
+    .attr('class', 'filter list-group-item');
+
+item.append('label').text('Date:');
+item.append('input')
+    .attr('data-key', 'datetime')
+    .attr('class', 'form-control form-control-sm');
+*/
+
+let control = controls[2];
+
+
+function construct_control(control)
+{
+    switch(control.type) {
+    case 'choices':
+        let button, menu, option, item, attrs;
+
+        // add container for new dropdown selector
+        item = filters.select('#select').append('div').attr('class', 'dropdown');
+    
+        // create dropdown selector toggle button
+        button = item.append('button');
+        attrs = { 'id': `${control.key}-button`,
+                  'type': 'button',
+                  'class': 'dropdown-toggle btn btn-default btn-sm',
+                  'aria-haspopup': 'true',
+                  'aria-expanded': 'false',
+                  'data-toggle': 'dropdown' };
+    
+        // set button attributes
+        Object.entries(attrs).forEach(([k,v]) => button.attr(k,v))
+    
+        // add button label and icon
+        button.html(`${control.label} `)
+            .append('span')
+            .attr('class', 'glyphicon glyphicon-chevron-right'); 
+    
+        // create dropdown selector menu
+        menu = item.append('div');
+        attrs = { 'class': 'dropdown-menu',
+                  'aria-labelledby': `${control.key}-button` };
+    
+        // set menu attributes
+        Object.entries(attrs).forEach(([k,v]) => menu.attr(k,v))
+
+        // add checkboxs for elements of `control.values`
+        control.values.forEach(
+            value => {
+                option = menu.append('a');
+                attrs = { 'data-key': control.key,
+                          'data-value': value,
+                          'href': '#',
+                          'class':
+                             'dropdown-item form-control form-control-sm' };
+
+                // set menu option attributes
+                Object.entries(attrs).forEach(([k,v]) => option.attr(k,v))
+                
+                // add label and checkbox
+                option.append('input').attr('type', 'checkbox');
+                option.append('label').html(`&nbsp;${value.toUpperCase()}`);
+            }
+        );
+        break;
+
+    case 'text':
+        let item, input, label, attrs;
+        
+        item = filters.append('li').attr('class', 'list-group-item');
+        
+        label = item.append('label').html(`${control.label} `)
+        input = item.append('input');
+        attrs = { 'data-key': control.key,
+                  'type': 'text',
+                  'placeholder': control.placeholder,
+                  'class': 'form-control form-control-sm' };
+        Object.entries(attrs).forEach(([k,v])=>input.attr(k,v));
+        break;
+    
+    default:
+        break;
+    }
+}
+
+
+/* Setup Event Handlers */
+d3.select("#filter-btn").on('click', apply_filter_onclick);
+d3.select('#filter-form').on('submit', apply_filter_onclick);
+d3.selectAll('.dropdown-menu>a').on('click', menu_option_onclick);
 
 // populate table with initial data
-populate_table();
+update_table();
 
