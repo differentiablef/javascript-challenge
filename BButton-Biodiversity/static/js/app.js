f59a51346eee7ccdbab41cb25384e398d10b7ca8
Michael Blackmon <differentiablef@gmail.com>
2019-10-12 15:34:29 -0400

    belly button rough out

--- BButton-Biodiversity/static/js/app.js
+++ BButton-Biodiversity/static/js/app.js
@@ -1,8 +1,8 @@
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
+    // @TODO: Complete the following function that builds the metadata panel
 
-  // Use `d3.json` to fetch the metadata for a sample
+    // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
 
     // Use `.html("") to clear any existing metadata
@@ -15,42 +15,84 @@ function buildMetadata(sample) {
     // buildGauge(data.WFREQ);
 }
 
-function buildCharts(sample) {
+function buildCharts(sample_id)
+{
+    let selector = d3.select('#selDataset');
+    
+    // @TODO: Use `d3.json` to fetch the sample data for the plots
+    d3.json(`/samples/${sample_id}`).then(
+        function (sample) {
+            console.log(`Processing data for sample ${sample_id}`);
+            let bubble = d3.select('#bubble'),
+                pie = d3.select('#pie'),
+                gauge = d3.select('#gauge');
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+            // @TODO: Build a Bubble Chart using the sample data
+            let trace = {
+                x: sample.otu_ids,
+                y: sample.sample_values,
+                text: sample.otu_labels,
+                mode: 'markers',
+                marker: {
+                    color: sample.otu_ids,
+                    size: sample.sample_values
+                    //colorscale: "flat"
+                },
+                type: 'scatter'
+            };
 
-    // @TODO: Build a Bubble Chart using the sample data
+            let layout = {
+                title: `Sample ID ${sample_id}`
+            };
+            
+            Plotly.newPlot("bubble", [trace], layout);
+            // @TODO: Build a Pie Chart
+            // HINT: You will need to use slice() to grab the top 10 sample_values,
+            // otu_ids, and labels (10 each).
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+        }
+    );
+
+    console.log(
+        sample_id
+    );
 }
 
-function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
-
-  // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
+function init()
+{
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+    
+    // Use the list of sample names to populate the select options
+    d3.json("/names").then((sampleNames) => {
+        sampleNames.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+        
+        // Use the first sample from the list to build the initial plots
+        const firstSample = sampleNames[0];
+        buildCharts(firstSample);
+        buildMetadata(firstSample);
     });
-
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
 }
 
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
+function optionChanged(newSample)
+{
+    // Fetch new data each time a new sample is selected
+    buildCharts(newSample);
+    buildMetadata(newSample);
+}
+
+function selDataset_onchange()
+{
+    var sel = d3.select('#selDataset');
+    optionChanged(sel.property('value'));
 }
 
 // Initialize the dashboard
+d3.select('#selDataset').on('change', selDataset_onchange);
+
 init();
